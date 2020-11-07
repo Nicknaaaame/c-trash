3cb21cb8d5a3e94432f1d2537b488c13d37e32f1
Nicknaaaame <48119336+Nicknaaaame@users.noreply.github.com>
2020-11-07 15:34:35 +0700

    Update main.c

--- lab4/main.c
+++ lab4/main.c
@@ -12,29 +12,37 @@ void log(){
 }
 
 void solution(){
-    sem_t sem1;
-    sem_t sem2;
-    sem_init(&sem1,0, 1);
-    sem_init(&sem2,0, 0);
+    sem_t *sem1;
+    sem_t *sem2;
+    // sem_init(&sem1,0, 1);
+    // sem_init(&sem2,0, 0);
 
+    sem1 = sem_open("1", O_CREAT | O_EXCL, 0777, 1);
+    sem2 = sem_open("2", O_CREAT | O_EXCL, 0777, 0);
+
+    if(sem1==-1||sem2==-1){
+        printf("ERR");
+        return(-1);
+    }
+    
     pid_t pid1 = fork();
     pid_t pid2 = fork();
     int n = 5;
 
     if(pid1==0&&pid2>0) {
         for(int i = 0; i<n; i++){
-            sem_wait(&sem1);
+            sem_wait(sem1);
             log();
             printf("1------------------\n");
-            sem_post(&sem2);
+            sem_post(sem2);
         }
     }
     if(pid1>0 && pid2==0) {
         for(int i = 0; i<n; i++){
-            sem_wait(&sem2);
+            sem_wait(sem2);
             log();
             printf("2\n");
-            sem_post(&sem1);
+            sem_post(sem1);
         }      
     }
 }
