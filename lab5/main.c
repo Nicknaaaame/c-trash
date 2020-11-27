57ac010be14088784ca703bf6c105b938e9e6f3d
Nicknaaaame <48119336+Nicknaaaame@users.noreply.github.com>
2020-11-27 19:27:12 +0700

    Update main.c

--- lab5/main.c
+++ lab5/main.c
@@ -1,52 +1,55 @@
-#include<stdio.h> 
-#include<stdlib.h> 
-#include<unistd.h> 
-#include<sys/types.h> 
-#include<string.h> 
-#include<sys/wait.h> 
-#include<ctype.h>
-  
-int main() 
-{ 
-    int fd1[2];
-  
-    char input_str[100]; 
-    pid_t p; 
-  
-    if (pipe(fd1)==-1) { 
-        fprintf(stderr, "Pipe Failed" ); 
-        return 1; 
-    } 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <ctype.h>
 
-    scanf("%s", input_str); 
-    p = fork(); 
-  
-    if (p > 0) 
-    { 
-        char result_str[100]; 
-  
-        close(fd1[0]); 
-  
-        write(fd1[1], input_str, strlen(input_str)+1); 
-        close(fd1[1]); 
-    } 
-    else
-    { 
+char* str_toupper(char *str)
+{
+    int i;
+    for (i = 0; i < strlen(str); i++)
+        str[i] = toupper(str[i]);
+}
+
+int main()
+{
+    int fd1[2];
+
+    char input_str[100];
+
+    if (pipe(fd1) == -1)
+    {
+        fprintf(stderr, "Pipe Failed");
+        return 1;
+    }
+
+    scanf("%s", input_str);
+
+    if (fork())
+    {
+        printf("if");
+        close(fd1[0]);
+        write(fd1[1], input_str, strlen(input_str) + 1);
         close(fd1[1]);
-  
-        char result_str[100]; 
-        read(fd1[0], result_str, 100); 
-  
-        int k = strlen(result_str); 
-        int i; 
-        for (i=0; i<k; i++) 
-            result_str[i] = toupper(result_str[i]); 
-  
-        result_str[i] = '\0';
-  
+    }
+    else
+    {
+        printf("else");
+        close(fd1[1]);
+        char str[100];
+        read(fd1[0], str, 100);
+
+        char* result_str=str_toupper(str);
+
+        result_str[strlen(result_str)] = '\0';
+
         printf("Result string is %s", result_str);
 
-        close(fd1[0]); 
-        exit(0); 
-    } 
+        close(fd1[0]);
+        exit(0);
+    }
+    printf("end");
+
 }
